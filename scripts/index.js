1309c5ca2fa74eaaef0c2c9b73371e290c2a49bb
Axeliriya <axeliriya@yandex.ru>
2025-04-05 23:58:43 +0200

    Добавлены функции вывода карточек на страницу и их удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+const addCard = ({ name, link }, onDelete) => {
+  const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const deleteButton = cardItem.querySelector(".card__delete-button");
+
+  cardItem.querySelector(".card__image").src = link;
+  cardItem.querySelector(".card__image").alt = name;
+  cardItem.querySelector(".card__title").textContent = name;
+
+  deleteButton.addEventListener("click", () => onDelete(cardItem));
+
+  cardList.append(cardItem);
+};
 
 // @todo: Функция удаления карточки
+const deleteCard = (card) => {
+  card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+const renderInitialCards = (cards) => {
+  cards.forEach((card) => {
+    addCard(card, deleteCard);
+  });
+};
+
+renderInitialCards(initialCards);
